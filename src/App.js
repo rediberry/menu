597a7d21e275cfe30937ce8889208ac983880392
Eric Aguerreberry <eric_aguerreberry@hotmail.com>
2022-11-30 01:24:52 -0300

    fix categories

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
-const allCategories =['all',...new Set(items.map((item)=> item.category))] ;
+const allCategories =['todo',...new Set(items.map((item)=> item.category))] ;
 
 function App() {
 const [menuItems, setMenuItems] = useState(items);
 const [categories, setCategories] = useState(allCategories);
 
 const filterItems = (category) =>{
-  if (category === 'all'){
+  if (category === 'todo'){
     setMenuItems(items);
     return;
   }
